ada2a6ef3f7e93d49605005d354775d7b0f4b707
const-kiselev <kostyakiselev@gmail.com>
2020-09-22 13:29:21 +0300

    stable version

---
+++ index.php
@@ -0,0 +1,152 @@
+<?php
+
+// Валидация входных данных
+function inputValidation($xVal, $yVal, $rVal){
+    if(!is_numeric($xVal) || !is_numeric($yVal) || !is_numeric($rVal)) {
+
+        echo 'console.log("1");';
+        return false;
+    }
+    if($xVal < -4 || $xVal > 4){
+        echo 'console.log("2");';
+        return false;
+    }
+    if($yVal <= -5 || $yVal >= 3){
+        echo 'console.log("3");';
+        return false;
+    }
+    if($rVal <1 || $rVal > 5){
+        echo 'console.log("4");';
+        return false;
+    }
+    return true;
+}
+
+function intersection($xVal, $yVal, $rVal){
+    if($yVal >= 0 && $yVal<=-$rVal/2.0 && $xVal >=$rVal && $xVal >= 0)// прямоугольник
+        return true;
+    elseif($yVal<=0 && $yVal>= -$rVal/2.0) {
+        if ($xVal >= -$rVal / 2.0 && $xVal <= 0 && sqrt($xVal * $xVal + $yVal * $yVal)) // сегмент окружности
+            return true;
+    }
+    else{
+        $xWidth = abs(2* $yVal - $rVal);
+        if(   $xWidth -  abs($xVal) >=0 )
+            return true;
+    }
+    return false;
+
+}
+
+function main(){
+    $time_start = microtime(true);
+    // поверяем не пустой ли запрос
+    if (!empty($_POST))
+        retunr;
+    $x = $_POST["xValue"];
+    $y = $_POST["yValue"];
+    $r = $_POST["rValue"];
+    $timeID = $_POST["timeID"];
+
+    // проводим валидацию данных
+    if(!inputValidation($x, $y, $r)) {
+        echo "console.log('PHP validation error.');";
+        return;
+    }
+
+    $result = intersection($x, $y, $r);
+
+    $time_end = microtime(true);
+    $execution_time = ($time_end - $time_start) * 1000; // millisecond
+    echo "addNewValue('" .$timeID . "'," . $execution_time . "," . $x . "," .  $y . "," . $r . "," . ($result ? "1":"0") . ");";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script type="text/javascript" src="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <script>
+        let myCanvas;
+        window.onload = function(){
+            initElementsOfForm();
+            myCanvas = new Canv(200,100);
+            myCanvas.setVars(1,1,3);
+            myCanvas.draw();
+
+            <?php
+            main();
+            ?>
+            fillTable();
+
+        }
+        function drawMe(){
+            if(!formValidation())
+                return false;
+            myCanvas.setVars( Number(document.getElementById("xValue").getAttribute("value")),
+                Number(document.getElementById("yValue").getAttribute("value")),
+                Number(document.getElementById("rValue").getAttribute("value")) );
+            myCanvas.draw();
+            myCanvas.draw();
+        }
+    </script>
+
+</head>
+<body>
+<div id="container">
+    <div id="header">
+        <p>Лабораторная работа #1</p>
+        <p>Киселёв Константин</p>
+        <p>P3212<br/>Вариант 2327</p>
+    </div>
+    <div id="main">
+        <div id="canv">
+        </div>
+        <form name="myForm" action="index.php" onsubmit="return formValidation()" method="post">
+
+            <div id="xCheckboxes">
+                <label>X = </label>
+            </div>
+
+            <div id="yCheckboxes">
+                <label id="yLab" for="y">Y = </label>
+                <input id="y" name="yText" type="text"/>
+            </div>
+
+            <div id="rCheckboxes">
+                <label id="rLab" >R = </label>
+            </div>
+            <input type="button" value="Draw" onclick="drawMe()">
+            <input type="hidden" id="xValue" name="xValue" value="">
+            <input type="hidden" id="yValue" name="yValue" value="">
+            <input type="hidden" id="rValue" name="rValue" value="">
+            <input type="hidden" id="timeID" name="timeID" value="">
+            <input type="submit">
+        </form>
+        <table id="results">
+            <tr>
+                <th>#</th>
+                <th>Time</th>
+                <th>Exec time (ms)</th>
+                <th>x</th>
+                <th>y</th>
+                <th>r</th>
+                <th>Result</th>
+            </tr>
+
+        </table>
+    </div>
+    <div id="footer">
+
+    </div>
+</div>
+
+
+
+
+
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,213 @@
+function initElementsOfForm() {
+    const xDiv = document.getElementById("xCheckboxes");
+    const rDiv = document.getElementById("rCheckboxes");
+    let i, j;
+    for(i = -4, j=0; i < 5; i++, j++){
+        let input = document.createElement("input");
+        input.setAttribute("id", "x"+ j.toString());
+        input.setAttribute("value", i);
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("name", "xCheckbox");
+        let label = document.createElement("label");
+        label.setAttribute("for", input.getAttribute("id"));
+        label.innerText = i.toString() ;
+        xDiv.appendChild(input);
+        xDiv.appendChild(label);
+    }
+    for(i=1; i<6; i++){
+        let input = document.createElement("input");
+        input.setAttribute("type", "button");
+        input.setAttribute("value", i);
+        input.setAttribute("name", "yButton");
+        input.setAttribute("onclick", "changeY(" + i.toString() +")");
+        rDiv.appendChild(input);
+
+    }
+}
+
+function addNewValue(timeID, exeTime, x,y,r,result){
+    // проверка на сущестование подобного значения
+    if(!localStorage.getItem(timeID))
+        window.localStorage.setItem(timeID, exeTime+" "+x+" "+y+" "+r+" "+result );
+}
+
+function fillTable(){
+    let table = document.getElementById("results");
+    let lsSize = window.localStorage.length;
+    console.log(lsSize);
+    console.log(window.localStorage);
+    for(let i=0; i<lsSize; i++) {
+        let key = window.localStorage.key(i);
+        let row = table.insertRow(i+1);
+
+        // let cell = row.insertCell(0);
+        let data = window.localStorage.getItem(key).split(" ");
+        row.insertCell(0).innerText = i+1;
+        let dd =  new Date(key);
+        row.insertCell(1).innerHTML = (new Date( Number(key) )).toString().split(" ")[4];
+        data.forEach(function(item, i){
+            let cell = row.insertCell(i+2);
+            switch (i){
+
+                case 4:{
+                    if(item == "1")
+                        cell.innerHTML = '<p class=\"green\">IN</p>';
+                    else
+                        cell.innerHTML = "<p class=\"red\">OUT</p>";
+                    break;
+                }
+                default:
+                    cell.innerHTML = item;
+
+            }
+
+        });
+
+
+    }
+}
+function getPrevInput(){
+
+}
+
+function changeY(rVal) {
+    // document.getElementById("rLab").innerHTML = "r = " + rVal;
+    document.getElementById("rValue").setAttribute("value", rVal);
+    let yButtons = document.getElementsByClassName("yButton");
+    for(let i=0; i< yButtons.length; i++)
+    {
+        yButtons[i].setAttribute("checked", false);
+    }
+}
+
+function formValidation() {
+    // Y:
+    // проверяем есть ли `,` -> `.`
+    // Число (float, int) ли это?!
+    document.getElementById("timeID").setAttribute("value", Date.now().toString());
+    let y = document.forms["myForm"]["yText"].value;
+    if(y.indexOf(","))
+        y.replace(",","."), document.forms["myForm"]["yText"].value = y;
+    y = Number(y);
+    Number.isNaN(y);
+    if (Number.isNaN(y) || (y<=-5 || y>=3)) {
+        alert("Y value is not valid.");
+        return false;
+    }
+    document.getElementById("yValue").setAttribute("value", y);
+
+    let xCount = document.forms["myForm"]["xCheckbox"].length;
+    let checkedCount = 0;
+    let x;
+    for(let i = 0; i<xCount; i++){
+        if(document.forms["myForm"]["xCheckbox"][i].checked == true) {
+            checkedCount++;
+            x = Number(document.forms["myForm"]["xCheckbox"][i].value);
+            if(checkedCount > 1) {
+                alert("X value is not valid.");
+                return false;
+            }
+        }
+    }
+    document.getElementById("xValue").setAttribute("value", x);
+
+    let r = document.getElementById("rValue").getAttribute("value");
+    if(r == "" || r>"5" || r<"1"){
+        alert("R value is not valid.");
+        return false;
+    }
+    return true;
+
+
+}
+
+class Canv {
+    #m_center;
+    #m_canvas;
+    #m_unit;
+    #m_point;
+    #m_settings;
+
+constructor(width, height){
+        this.#m_canvas = document.createElement("CANVAS");
+        this.#m_canvas.setAttribute("width", width);
+        this.#m_canvas.setAttribute("height", height);
+        document.getElementById("canv").appendChild(this.#m_canvas);
+        this.#m_center = {x: width / 2.0 ,  y: height / 2.0};
+        this.#m_unit= width / 16.0;
+    }
+
+    #m_drawAxis = function(){
+
+        let ctx = this.#m_canvas.getContext('2d');
+        ctx.fillStyle = 'black';
+        ctx.moveTo(0, this.#m_canvas.height / 2.0);
+        ctx.lineTo(this.#m_canvas.width, this.#m_canvas.height / 2.0);
+        ctx.stroke();
+        ctx.moveTo(this.#m_canvas.width / 2.0, 0);
+        ctx.lineTo(this.#m_canvas.width / 2.0, this.#m_canvas.height);
+        ctx.stroke();
+    };
+    #m_drawUnits = function(){
+        let ctx = this.#m_canvas.getContext('2d');
+        ctx.fillStyle = 'black';
+
+        // if (canvas.getContext) {
+        ctx.moveTo(this.#m_center.x + this.#m_unit, this.#m_center.y - 4);
+        ctx.lineTo(this.#m_center.x + this.#m_unit, this.#m_center.y + 4);
+        ctx.stroke();
+
+        ctx.moveTo(this.#m_center.x - 4, this.#m_center.y - this.#m_unit);
+        ctx.lineTo(this.#m_center.x + 4, this.#m_center.y - this.#m_unit);
+        ctx.stroke();
+    };
+    #m_drawShapes = function(){
+        let ctx = this.#m_canvas.getContext('2d');
+        {
+            ctx.fillStyle = 'blue';
+            ctx.moveTo(this.#m_center.x, this.#m_center.y)
+            let radius = this.#m_settings.r/2.0 * this.#m_unit;
+            ctx.arc(this.#m_center.x, this.#m_center.y,
+                radius, Math.PI,0.5*Math.PI , true);
+            ctx.fill();
+        }
+        {
+            ctx.moveTo( this.#m_center.x,  - this.#m_settings.r / 2.0 * this.#m_unit);
+            ctx.rect(this.#m_center.x,  this.#m_center.y, this.#m_settings.r  * this.#m_unit, this.#m_settings.r / 2.0 * this.#m_unit);
+            ctx.fill();
+        }
+        {
+            ctx.beginPath();
+            ctx.moveTo( this.#m_center.x,  this.#m_center.y );
+            ctx.lineTo( this.#m_center.x, this.#m_center.y - this.#m_settings.r / 2.0  * this.#m_unit );
+            ctx.lineTo( this.#m_center.x - this.#m_settings.r  * this.#m_unit, this.#m_center.y );
+            ctx.fill();
+        }
+        {
+            ctx.fillStyle = 'red';
+            let unit = this.#m_unit *0.2;
+            ctx.moveTo(this.#m_center.x + this.#m_point.x * this.#m_unit - unit*0.5  ,this.#m_center.y - this.#m_point.y * this.#m_unit - unit*0.5);
+            ctx.fillRect(this.#m_center.x + this.#m_point.x * this.#m_unit - unit*0.5  ,this.#m_center.y - this.#m_point.y * this.#m_unit - unit*0.5,unit,unit); // fill in the pixel at (10,10)
+        }
+    };
+
+
+    draw(){
+        let ctx = this.#m_canvas.getContext('2d');
+        ctx.clearRect(0, 0, this.#m_canvas.width, this.#m_canvas.height);
+        this.#m_drawAxis();
+        this.#m_drawUnits();
+        this.#m_drawShapes();
+
+    }
+    setVars(xVar,yVar,rVar){
+        // после получения значений x, y, r необходимо определить максимальное и после этого изсенить масштаб единицы
+        this.#m_point = {x:xVar, y: yVar};
+        this.#m_settings= {r:rVar};
+    }
+
+
+
+
+
+}
